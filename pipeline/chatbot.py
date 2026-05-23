bf64d51eb393bd5127a2bce21fe7691665aff8c4
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-05-23 12:42:13 +0530

    Chat with AI enabled

--- pipeline/chatbot.py
+++ pipeline/chatbot.py
@@ -1,5 +1,14 @@
 import chromadb
 from chonkie.embeddings import SentenceTransformerEmbeddings
+from pydantic_ai import Agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(  
+  'openai:gpt-4o-mini',
+  instructions="You are an expert internal company AI assistant. Your job is to answer user questions using ONLY the provided document context below. If the context does not contain the answer, say 'I cannot find that in the documents.Do not invent facts outside of the provided context.",
+)
 
 def retrieve_the_doc(query):
     chroma_client = chromadb.PersistentClient(path="./chroma_db")
@@ -10,3 +19,10 @@ def retrieve_the_doc(query):
     results = collection.query(query_embeddings=[query_embedding], n_results=3)
 
     return "\n\n".join(results["documents"][0])
+
+def chat_with_doc(query):
+    context = retrieve_the_doc(query)
+    prompt = f"Context: {context}\n\nQuestion: {query}\nAnswer:"
+    result = agent.run_sync(prompt)
+    return result.output
+
--- pipeline/main.py
+++ pipeline/main.py
@@ -1,4 +1,7 @@
 from ingest import ingest_doc
+from chatbot import chat_with_doc
 
 if __name__ == "__main__":
-    ingest_doc()
+    # ingest_doc()
+    chatbot_response = chat_with_doc(input("Ask a question about the TMS document: "))
+    print("Chatbot response:", chatbot_response)
