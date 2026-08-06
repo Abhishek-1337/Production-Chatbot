84e5b1a05b5339b9e1913ead1fc7e9e5fc29d124
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-08-07 00:00:18 +0530

    parser updated

--- backend/services/parser.py
+++ backend/services/parser.py
@@ -2,6 +2,10 @@ from fastapi import Depends, FastAPI, File, HTTPException, UploadFile
 from services.pdf_parser import pdf_parser
 
 def parser(file: UploadFile = File(...)):
-    if file.content_type == "application/pdf":
-        print("got here")
-        pdf_parser(file)
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are supported."
+        )
+
+    return pdf_parser(file)
