cd5fcf6df1bf6e1a38957fa5ae85ea87a8a4f962
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-08-07 00:49:13 +0530

    chunking improved

--- backend/services/ingest.py
+++ backend/services/ingest.py
@@ -5,33 +5,43 @@ import chromadb
 
 
 def ingest_doc(data): 
-    md = MarkItDown()
-    text_bytes = data.encode("utf-8")
-    result = md.convert_stream(BytesIO(text_bytes), file_extension=".txt")
+    try:
+        md = MarkItDown()
+        text_bytes = data.encode("utf-8")
+        result = md.convert_stream(BytesIO(text_bytes), file_extension=".txt")
+    
+        pipe = (
+            Pipeline()
+            .chunk_with("recursive", tokenizer="gpt2", chunk_size=200, recipe="markdown")
+            # .chunk_with("semantic", chunk_size=512)
+            .refine_with("overlap", context_size=50)
+            .refine_with("embeddings", embedding_model="sentence-transformers/all-MiniLM-L6-v2")
+        )
+    
+        doc = pipe.run(result.markdown)
+    
+        chroma_client = chromadb.PersistentClient(path="./chroma_db")
+        collection = chroma_client.get_or_create_collection(name="documents")
+    
+        embeddings_list = []
+        ids_list = []
+        documents_list = []
+        for idx, chunk in enumerate(doc.chunks):
+            documents_list.append(chunk.text)
+            ids_list.append(str(idx))
+            embeddings_list.append(chunk.embedding)
+    
+        collection.add(
+            documents=documents_list,
+            ids=ids_list,
+            embeddings=embeddings_list
+        )
+    
+        return {
+            
+        }
+    except Exception as e:
+        raise RuntimeError(f"Error: {e}") from e
 
-    pipe = (
-        Pipeline()
-        .chunk_with("recursive", tokenizer="gpt2", chunk_size=600, recipe="markdown")
-        .chunk_with("semantic", chunk_size=512)
-        .refine_with("overlap", context_size=128)
-        .refine_with("embeddings", embedding_model="sentence-transformers/all-MiniLM-L6-v2")
-    )
-
-    doc = pipe.run(result.markdown)
-
-    chroma_client = chromadb.PersistentClient(path="./chroma_db")
-    collection = chroma_client.get_or_create_collection(name="documents")
-
-    embeddings_list = []
-    ids_list = []
-    documents_list = []
-    for idx, chunk in enumerate(doc.chunks):
-        documents_list.append(chunk.text)
-        ids_list.append(str(idx))
-        embeddings_list.append(chunk.embedding)
-
-    collection.add(
-        documents=documents_list,
-        ids=ids_list,
-        embeddings=embeddings_list
-    )
+    # all_data = collection.get()
+    # print(all_data)
