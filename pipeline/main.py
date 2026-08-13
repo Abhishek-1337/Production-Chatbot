8ebf20d07ec8ac623d29ccda4fae11aa89718c82
Abhishek-1337 <abhishekrinku4@gmail.com>
2026-08-14 01:35:38 +0530

    removed pipeline folder after integrating with backend

--- pipeline/main.py
+++
@@ -1,7 +0,0 @@
-from ingest import ingest_doc
-from chatbot import chat_with_doc
-
-if __name__ == "__main__":
-    # ingest_doc()
-    chatbot_response = chat_with_doc(input("Ask a question about the TMS document: "))
-    print("Chatbot response:", chatbot_response)
